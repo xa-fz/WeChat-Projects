21209f976b9add7aae9fcb7485da149e14d594fc
xa-fz <553866469@qq.com>
2020-11-20 00:12:39 +0800

    学会使用wx.request请求公共接口

--- weatherDemo/pages/index/index.js
+++ weatherDemo/pages/index/index.js
@@ -6,33 +6,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    myCity: ''
+    region: ['陕西省', '西安市', '新城区'],
+    now: {}
+  },
+
+  getWeather: function () {
+    let that = this;
+    wx.request({
+      url: 'https://free-api.heweather.com/s6/weather/now',
+      data: {
+        location: that.data.region[1],
+        key: 'dd19f274c3074b2c9c416fb4292ef197'
+      },success: function (res) {
+        console.log(res.data);
+        that.setData({
+          now: res.data.HeWeather6[0].now
+        })
+      }
+    })
+  },
+
+  regionChange: function (e) {
+    this.setData({
+      region: e.detail.value
+    });
+    this.getWeather();
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-        myCity: app.globalData.userInfo.city.includes('an') && app.globalData.userInfo.city.includes('Xi') && '西安市'
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true,
-          myCity: res.userInfo.city.includes('an') && res.userInfo.city.includes('Xi') && '西安市'
-        })
-      }
-    }
+    this.getWeather();
   },
 
   /**
