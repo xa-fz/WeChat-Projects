93cf9bcb9cec43c73f946e5f77d09bbc48de1419
xa-fz <553866469@qq.com>
2020-11-29 12:54:52 +0800

    获取当前经纬度信息

--- weatherDemo/pages/index/index.js
+++ weatherDemo/pages/index/index.js
@@ -7,15 +7,49 @@ Page({
    */
   data: {
     region: ['陕西省', '西安市', '新城区'],
-    now: {}
+    now: {},
+    LongLat: ''
   },
-
-  getWeather: function () {
+getLocation: function () {
+  wx.getLocation({
+    type: 'wgs84',
+    success: res => {
+      const {
+        latitude,
+        longitude
+      } = res
+     console.log(latitude,longitude);
+     this.getWeather(`${longitude},${latitude}`);
+     this.setData({
+     'LongLat': `${longitude},${latitude}`
+     })
+    },
+    fail: res => {
+      // console.log('您已经拒绝获取定位，需要打开设置')
+      wx.showModal({
+        title: '警告',
+        content: '如果不授权获取您的定位，将无法为您展示你当前位置天气详情.',
+        confirmText: '去授权',
+        showCancel: false,
+        success: res => {
+          if (res.confirm) {
+            wx.openSetting({
+              success: res => {
+                this.getLoaction()
+              }
+            })
+          }
+        }
+      })
+    }
+  })
+},
+  getWeather: function (data) {
     let that = this;
     wx.request({
       url: 'https://free-api.heweather.com/s6/weather/now',
       data: {
-        location: that.data.region[1],
+        location: data ? data : that.data.region[1],
         key: 'dd19f274c3074b2c9c416fb4292ef197'
       },success: function (res) {
         console.log(res.data);
@@ -37,7 +71,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getWeather();
+    this.getLocation();
   },
 
   /**
